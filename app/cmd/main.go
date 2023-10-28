439f88b7c1ad600c1e73eb225b7f0bb402dfee1f
kir1ll0909 <kir1ll0909@axarea.ru>
2023-10-28 18:53:31 +0300

    add config

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"db-backuper/config"
+	"log"
+)
+
 func main() {
+	cfgFile, err := config.LoadConfig()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	cfg, err := config.ParseConfig(cfgFile)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	log.Println("Config loaded")
+
+	log.Printf("%+v", cfg)
 
 }
---
+++ config/config.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+	"fmt"
+	"github.com/go-playground/validator/v10"
+	"github.com/pkg/errors"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Postgres Postgres
+}
+
+type Postgres struct{}
+
+func LoadConfig() (*viper.Viper, error) {
+	v := viper.New()
+
+	v.AddConfigPath(fmt.Sprintf("./%s", ConfigPath))
+	v.SetConfigName(ConfigFileName)
+	v.SetConfigType(ConfigExtension)
+	if err := v.ReadInConfig(); err != nil {
+		err = errors.Wrap(err, "config.LoadConfig")
+		return nil, err
+	}
+
+	return v, nil
+}
+
+func ParseConfig(v *viper.Viper) (*Config, error) {
+	var c Config
+
+	err := v.Unmarshal(&c)
+	if err != nil {
+		err = errors.Wrap(err, "config.ParseConfig")
+		return nil, err
+	}
+
+	err = validator.New().Struct(c)
+	if err != nil {
+		return nil, err
+	}
+
+	return &c, nil
+}
